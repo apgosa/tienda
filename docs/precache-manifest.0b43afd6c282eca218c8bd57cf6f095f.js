13aee86977c51a6abb0ffad4684de87d151628e1
apgosa <68440562+apgosa@users.noreply.github.com>
2022-06-13 21:33:01 -0600

    Add files via upload

---
+++ docs/precache-manifest.0b43afd6c282eca218c8bd57cf6f095f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f25d3b688858159ebd7b",
+    "url": "/static/css/main.058a23fc.chunk.css"
+  },
+  {
+    "revision": "f25d3b688858159ebd7b",
+    "url": "/static/js/main.f25d3b68.chunk.js"
+  },
+  {
+    "revision": "81b5cea7f790844d79af",
+    "url": "/static/js/1.81b5cea7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df76d821abd702bf2bcf72a2831b420d",
+    "url": "/index.html"
+  }
+];
